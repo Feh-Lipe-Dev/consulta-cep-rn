4c13864f40a6d39c2c78618ed9def372a19c8f80
Felipe Goncalves <felipe.cleia05@gmail.com>
2024-09-18 19:37:25 -0300

    criando repositorio

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StatusBar, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './style.js';
+import { useState } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function App() {
+
+  const [cep, setCep] = useState("");
+  const [logradouro, setLogradouro] = useState("");
+  const [localidade, setLocalidade] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [estado, setEstado] = useState("");
+
+  async function consultarCep() {
+    const req = await fetch("https://viacep.com.br/ws/" + cep + "/json/");
+    const devolve = await req.json();
+
+    setLogradouro("Endereço: " + devolve.logradouro);
+    setBairro("Bairro: " + devolve.bairro);
+    setLocalidade("Cidade: " + devolve.localidade);
+    setEstado("Estado: " + devolve.estado);
+  }
+
+  return <SafeAreaView style={styles.container}>
+    <StatusBar barStyle="default" />
+
+    <Image style={styles.img} source={
+      require("./assets/lupa-cep.jpeg")
+    }/>
+
+    <View style={styles.form}>
+      <TextInput placeholder='Digite o CEP...'
+      style={styles.inputCep} 
+      onChangeText={texto => setCep(texto)} />
+
+      {/*<Button title='Consultar' onPress={consultarCep}/>*/ }
+
+      <TouchableOpacity style={styles.btn} onPress={consultarCep}>
+        <Text style={styles.btnText}>Consultar</Text>
+      </TouchableOpacity>
     </View>
-  );
+
+    <View style={styles.form}>
+      <Text style={styles.result}>{logradouro}</Text>
+      <Text style={styles.result}>{bairro}</Text>
+      <Text style={styles.result}>{localidade}</Text>
+      <Text style={styles.result}>{estado}</Text>
+    </View>
+  </SafeAreaView>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ style.js
@@ -0,0 +1,46 @@
+export const styles = {
+    container: {
+        backgroundColor: "#fff",
+        flex: 1,
+        alignItems: "center",
+        marginTop: 40
+    },
+    inputCep: {
+        backgroundColor: "#ececec",
+        padding: 15,
+        width: 150,
+        borderRadius: 6,
+        marginBottom: 2
+    },
+    form: {
+        marginTop: 20,
+    },
+    result: {
+        fontSize: 20,
+        fontWeight: 600,
+        padding: 7,
+        backgroundColor: "#f3d500",
+        minWidth: 300,
+        maxWidth: "auto",
+        minHeight: 1,
+        maxHeight: "auto",
+        borderRadius: 8,
+        marginTop: 2
+    },
+    btn: {
+        backgroundColor: "#2993c8",
+        width: 150,
+        borderRadius: 50,
+        padding: 15,
+        alignItems: "center",
+        marginTop: 10
+    },
+    btnText: {
+        color: "#fff",
+        fontSize: 20
+    },
+    img: {
+        with: 100,
+        height: 180
+    }
+}
